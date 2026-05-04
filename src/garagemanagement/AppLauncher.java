d4ac3484b5586c04100db47c3894ccc6a3b6dbd6
Trinh Nhu Hinh <nhuhinhtrinh@gmail.com>
2026-05-04 22:34:42 +0700

    main update & add applauncher

---
+++ src/garagemanagement/AppLauncher.java
@@ -0,0 +1,8 @@
+package garagemanagement;
+
+public class AppLauncher {
+    public static void main(String[] args) {
+        // Gọi hàm main của lớp Main JavaFX
+        Main.main(args); 
+    }
+}
